0cc67a2e664ae013d6e52c9bc718da4463506e52
Brandon Mathis <bemathis@gmail.com>
2020-11-13 00:21:40 -0500

    Fix controls

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -6,7 +6,7 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Dropdown,
+  UncontrolledDropdown,
 } from "reactstrap";
 
 const propTypes = {
@@ -16,68 +16,56 @@ const propTypes = {
 const Controls = ({ raiseHand }) => {
   const [currentCamera, setCurrentCamera] = useState(1);
   const [currentMic, setCurrentMic] = useState(1);
-  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [isRecording, setIsRecording] = useState(false);
-
-  const toggle = () => setDropdownOpen((prevState) => !prevState);
 
   return (
     <div className="controls">
       <div className="controls__cell">
-        <div>
-          <Button
-            color="warning"
-            size="lg"
-            onClick={() => {
-              raiseHand(true);
+        <Button color="danger">
+          <i class="fas fa-sign-out-alt fa-flip-horizontal"></i> Leave
+        </Button>
+        <Button
+          color="warning"
+          onClick={() => {
+            raiseHand(true);
 
-              setTimeout(() => {
-                raiseHand(false);
-              }, 2000);
-            }}
-          >
-            <i className="far fa-hand-paper"></i>
-          </Button>
-        </div>
-      </div>
-      <div className="controls__cell">
-        <div>
-          <Button
-            color="white"
-            onClick={() => setIsRecording(!isRecording)}
-            style={{
-              backgroundColor: isRecording && "crimson",
-              color: isRecording && "white",
-              transition: "color",
-            }}
-          >
-            <i
-              className="fas fa-circle"
-              style={{ color: isRecording ? "white" : "crimson" }}
-            ></i>{" "}
-            Start Recording
-          </Button>
-        </div>
-        <div
-          style={{
-            height: "100%",
-            display: "flex",
-            flexFlow: "column",
-            alignItems: "center",
-            fontSize: "0.75em",
+            setTimeout(() => {
+              raiseHand(false);
+            }, 2000);
           }}
         >
-          <div>
-            <i className="fas fa-user-secret"></i> Enter Sidebar
-          </div>
-          <ButtonGroup>
-            <Button color="info">1</Button>
-            <Button color="info">2</Button>
-            <Button color="info">3</Button>
-          </ButtonGroup>
-        </div>
-        <Dropdown isOpen={dropdownOpen} toggle={() => {}}>
-          <DropdownToggle color="info" caret onClick={() => toggle()}>
+          <i className="far fa-hand-paper"></i>
+        </Button>
+      </div>
+      <div className="controls__cell">
+        <Button
+          color="white"
+          onClick={() => setIsRecording(!isRecording)}
+          style={{
+            backgroundColor: isRecording && "crimson",
+            color: isRecording && "white",
+            transition: "color",
+          }}
+        >
+          <i
+            className="fas fa-circle"
+            style={{ color: isRecording ? "white" : "crimson" }}
+          ></i>{" "}
+          Start Recording
+        </Button>
+
+        <UncontrolledDropdown>
+          <DropdownToggle color="dark" caret>
+            <i class="fas fa-door-open"></i> Enter Sidebar
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem header>Enter Sidebar 1</DropdownItem>
+            <DropdownItem header>Enter Sidebar 2</DropdownItem>
+            <DropdownItem header>Enter Sidebar 3</DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+        <UncontrolledDropdown>
+          <DropdownToggle color="info" caret>
             Select Device
           </DropdownToggle>
           <DropdownMenu right>
@@ -86,29 +74,29 @@ const Controls = ({ raiseHand }) => {
               <span style={{ visibility: currentCamera !== 1 && "hidden" }}>
                 ✓
               </span>{" "}
-              🎥 Camera 1
+              <i class="fas fa-video"></i> Front Camera
             </DropdownItem>
             <DropdownItem onClick={() => setCurrentCamera(2)}>
               <span style={{ visibility: currentCamera !== 2 && "hidden" }}>
                 ✓
               </span>{" "}
-              🎥 Camera 2
+              <i class="fas fa-video"></i> Back Camera
             </DropdownItem>
             <DropdownItem header>Microphone</DropdownItem>
             <DropdownItem onClick={() => setCurrentMic(1)}>
               <span style={{ visibility: currentMic !== 1 && "hidden" }}>
                 ✓
               </span>{" "}
-              🎙 Built-in Microphone
+              <i class="fas fa-microphone-alt"></i> Built-in Microphone
             </DropdownItem>
             <DropdownItem onClick={() => setCurrentMic(2)}>
               <span style={{ visibility: currentMic !== 2 && "hidden" }}>
                 ✓
               </span>{" "}
-              🎙 Air Pods
+              <i class="fas fa-microphone-alt"></i> Air Pods
             </DropdownItem>
           </DropdownMenu>
-        </Dropdown>
+        </UncontrolledDropdown>
         <div>
           <Button color="danger">
             End Deposition <i className="fas fa-times"></i>

efe77f7b158e6d2d06d9cbcafa27953065c1dd24
Brandon Mathis <bemathis@gmail.com>
2020-11-13 00:10:35 -0500

    Fix secondary videos

--- src/components/VideoConference.jsx
+++ src/components/VideoConference.jsx
@@ -23,9 +23,6 @@ export default function VideoConference() {
         <div className="stream">
           <img alt="user video" src={maleVideo} />
         </div>
-        <div className="stream">
-          <img alt="user video" src={femaleVideo} />
-        </div>
       </div>
     </div>
   );
